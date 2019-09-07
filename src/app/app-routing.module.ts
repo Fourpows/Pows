118969f1ea67151961cf1ce63556e66708fabb0a
Mahmoud Kamal <mahmodkamal167@gmail.com>
2019-09-07 15:09:11 +0200

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
+
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path:'',
+    loadChildren:'./views/home/home/home.module#HomeModule'
+  },
+  {
+    path:'profile',
+    loadChildren:'./views/profile/profile/profile.module#ProfileModule'
+  },
+  {
+    path:'signup',
+    loadChildren:'./views/sign-up/sign-up/sign-up.module#SignUpModule'
+  },
+  {
+    path:'login',
+    loadChildren:'./views/login/login/login.module#LoginModule'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
