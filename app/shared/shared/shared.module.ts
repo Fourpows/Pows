1aa8c2e737a583bd75bb3a0fffea57e6d7e4d70e
Mahmoud Kamal <mahmodkamal167@gmail.com>
2019-09-07 17:15:54 +0200

    Auto-generated commit

---
+++ app/shared/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTabsModule} from '@angular/material/tabs';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatTabsModule,
+    MatToolbarModule,
+  ]
+})
+export class SharedModule { }
